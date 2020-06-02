2a13f38d35b3fdbb176c1e7234894d234c31411b
David <david.he07@yahoo.ca>
2020-06-02 18:24:00 -0400

    included code for authentication in models for user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,16 @@
 class User < ActiveRecord::Base
   has_secure_password
 
+  def self.authenticate_with_credentials(email, password)
+    user = User.find_by_email(email)
+  if user
+    if user.authenticate(password)
+      user
+    else
+      nil
+    end
+  else
+    nil
+  end
+end
 end
--- spec/user_spec.rb
+++ spec/user_spec.rb
@@ -5,6 +5,10 @@ RSpec.describe User, type: :model do
         # user{User.new(first_name: "Joe", last_name: "Smith", email: 'joe.smith@yahoo.ca', password: "jungle", password_confirmation: "jungle")}
     user = User.new
     
+    # user.first_name = "Joe"
+
+    # user.last_name = "Smith"
+
     user.name = "Joe Smith"
 
     user.email= 'joe.smith@yahoo.ca'
@@ -22,10 +26,15 @@ RSpec.describe User, type: :model do
       expect(user.password).to_not eq(user.password_confirmation)
     end
 
-    it 'is not valid when first_name is nil' do
+    it 'is not valid when name is nil' do
       user.name = nil;
       expect(user).not_to be_valid
     end
+
+    # it 'is not valid when last_name is nil' do
+    #   user.last_name = nil;
+    #   expect(user).not_to be_valid
+    # end
 
     it 'is not valid when email is nil' do
       user.email = nil;
@@ -41,8 +50,7 @@ RSpec.describe User, type: :model do
   
   describe '.authenticate_with_credentials' do
     # examples for this class method here
-    
-  end
 
+  end
 
 end
