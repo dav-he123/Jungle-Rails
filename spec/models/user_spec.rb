acfd65b3cac5bebd189074add80f2fbf92db0246
David <david.he07@yahoo.ca>
2020-06-02 12:56:43 -0400

    moved product and spec file into models folder

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,20 @@
+# require "rails_helper"
+
+# RSpec.describe User, type: :model do
+
+#   describe 'Validations' do
+
+#     it 'product is valid' do
+#       category = Category.new(name: "Microsoft Products")
+#       product = Product.new(password: jungle, password_confirmation: jungle, email: test@test.com, first_name=David, last_name=He)
+#       expect(product).to be_valid
+#     end
+
+#     it 'is not valid without password' do
+#       category = Category.new(name: "Microsoft Products")
+#       product = Product.new(password: nil, password_confirmation: jungle, email: test@test.com, first_name: David, last_name: He)
+#       expect(product).not_to be_valid
+#     end
+
+#   end
+# end
