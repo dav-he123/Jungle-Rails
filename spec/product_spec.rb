ff36f188bfe5f053d88911fff819ba8610c8b29e
David <david.he07@yahoo.ca>
2020-06-01 23:00:11 -0400

    changed price to price_cents in product spec file

--- spec/product_spec.rb
+++ spec/product_spec.rb
@@ -5,24 +5,24 @@ RSpec.describe Product, type: :model do
     # validation tests/examples here
     it 'product is valid' do
       category = Category.new(name: "Microsoft Products")
-      product = Product.new(name: "Microsoft Mouse", price: 40.00, quantity: 1, category: category)
+      product = Product.new(name: "Microsoft Mouse", price_cents: 4000, quantity: 1, category: category)
       expect(product).to be_valid
     end
     it 'is not valid without name' do
       category = Category.new(name: "Microsoft Products")
-      product = Product.new(name: nil, price: 40.00, quantity: 1, category: category)
+      product = Product.new(name: nil, price_cents: 4000, quantity: 1, category: category)
       expect(product).not_to be_valid
     end
 
-    it 'is valid with price' do
+    it 'is not valid without price' do
       category = Category.new(name: "Microsoft Products")
-      product = Product.new(name: "Microsoft Mouse", price: 40.00, quantity: 1, category: category)
-      expect(product).to be_valid
+      product = Product.new(name: "Microsoft Mouse", price_cents: nil, quantity: 1, category: category)
+      expect(product).not_to be_valid
     end
 
     it 'is not valid without quantity' do
       category = Category.new(name: "Microsoft Products")
-      product = Product.new(name: "Microsoft Mouse", price: 40.00, quantity: nil, category: category)
+      product = Product.new(name: "Microsoft Mouse", price_cents: 4000, quantity: nil, category: category)
       expect(product).not_to be_valid
     end
 
