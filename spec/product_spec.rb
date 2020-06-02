bde302977de6fcf30432e9f32ca07403ee7ecee5
David <david.he07@yahoo.ca>
2020-06-01 22:33:31 -0400

    made changes to validation testing for price

--- spec/product_spec.rb
+++ spec/product_spec.rb
@@ -5,19 +5,34 @@ RSpec.describe Product, type: :model do
     # validation tests/examples here
     it 'product is valid' do
       category = Category.new(name: "Microsoft Products")
-      product = Product.new(name: "Microsoft Mouse", price: 35.00, quantity: 1, category: category)
+      product = Product.new(name: "Microsoft Mouse", price: 40.00, quantity: 1, category: category)
       expect(product).to be_valid
     end
     it 'is not valid without name' do
       category = Category.new(name: "Microsoft Products")
-      product = Product.new(name: nil, price: 35.00, quantity: 1, category: category)
+      product = Product.new(name: nil, price: 40.00, quantity: 1, category: category)
       expect(product).not_to be_valid
     end
 
-    it 'is not valid without price' do
+    it 'is valid with price' do
       category = Category.new(name: "Microsoft Products")
-      product = Product.new(name: "Microsoft Mouse", price: nil, quantity: 1, category: category)
+      product = Product.new(name: "Microsoft Mouse", price: 40.00, quantity: 1, category: category)
+      expect(product).to be_valid
+    end
+
+    it 'is not valid without quantity' do
+      category = Category.new(name: "Microsoft Products")
+      product = Product.new(name: "Microsoft Mouse", price: 40.00, quantity: nil, category: category)
       expect(product).not_to be_valid
     end
+
+
+    it 'is not valid without category' do
+      category = Category.new(name: "Microsoft Products")
+      product = Product.new(name: "Microsoft Mouse", price: 40.00, quantity: 1, category: nil)
+      expect(product).not_to be_valid
+    end
+
+
   end
 end
